e132ae0c433c71bdcbd4dac5372e4bfd1c256f77
Brian Pennington <srsrec@gmail.com>
2017-04-26 12:05:40 -0400

    2nd commit
    Improved #4, still struggling with #2

--- main.js
+++ main.js
@@ -10,23 +10,24 @@ console.log(twoPlusTwo)
 
 // exercise 2
 
-function yell(string){
-	return string.toUpperCase();
-}
+// function yell(string){
+// 	return string.toUpperCase();
+// }
 
-//console.log(yell("hey there"))
+// console.log(yell("hey there"))
 
 function yell10() {
 	function yell(string){
-
+	
+	var string = "hey there"
 	var times = 10;
 	
 		for(var i=0; i < times; i++){
-	    	return string.toUpperCase();
+	    	string = string.toUpperCase();
 		}
-	}
+	} return
 }
-console.log(yell10(yell("hey there")))
+console.log(yell10())
 
 
 
@@ -43,35 +44,36 @@ function longest(a, b) {
 console.log(longest("ham sandwich", "pie"))
 console.log(longest("pi", "pie"))
 
-//  exercise 4
+ // exercise 4
 
-//function isVowel(x){
-//	if (x == 'a' || 'e' || 'i' || 'o' || 'u') {
-//		return true;
-//	} else {
-//		return false;
-//	}
-//}
+function isVowel(x){
+	if ((x == 'a') || (x == 'e') || (x =='i') || (x == 'o') || (x == 'u') || 
+		(x == 'A') || (x == 'E') || (x =='I') || (x == 'O') || (x == 'U')){
+		return true;
+	} else {
+		return false;
+	}
+}
 
 // I couldn't get my version with the Or Operator to work, so I copied Scott's, though I can't see how they aren't the same thing(?)
 
-function isVowel(x){
+// function isVowel(x){
 
-  if (x == 'a') {
-    return true;
-  } else if (x == 'e') {
-    return true;
-  } else if (x == 'i') {
-    return true;
-  } else if (x == 'o') {
-    return true;
-  } else if (x == 'u') {
-    return true;
-  } else {
-    return false;
-  }
+//   if (x == 'a') {
+//     return true;
+//   } else if (x == 'e') {
+//     return true;
+//   } else if (x == 'i') {
+//     return true;
+//   } else if (x == 'o') {
+//     return true;
+//   } else if (x == 'u') {
+//     return true;
+//   } else {
+//     return false;
+//   }
 
-}
+// }
 
-console.log(isVowel('a'))
+console.log(isVowel('A'))
 console.log(isVowel('b'))
